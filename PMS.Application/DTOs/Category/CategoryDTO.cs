3801426c135682e5d5a48605c4ad1329e13d9abf
minhtran291 <minhtran2912003@gmail.com>
2025-10-13 10:05:33 +0700

    Add Application/DTOs Admin, Category, Product

---
+++ PMS.Application/DTOs/Category/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PMS.Application.DTOs.Category
+{
+    public class CategoryDTO
+    {
+        public int CategoryID { get; set; }
+        [Required(ErrorMessage = "Tên của loại sản phẩm là bắt buộc")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Đảm bảo 6 ký tự")]
+        public required string Name { get; set; }
+        [StringLength(300, ErrorMessage = "Mô tả loại sản phẩm không được vượt quá 300 ký tự")]
+        public string? Description { get; set; }
+    }
+}
