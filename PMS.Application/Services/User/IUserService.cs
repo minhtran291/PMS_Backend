3883a20b39b84f4dc5d3ae6f5fa04c7e9c873964
minhtran291 <minhtran2912003@gmail.com>
2025-10-13 08:42:13 +0700

    Add Email, User, Token, Login Service

---
+++ PMS.Application/Services/User/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using PMS.Core.Domain.Constant;
+using PMS.Core.Domain.Identity;
+using PMS.Core.DTO.Content;
+using PMS.Core.DTO.Request;
+
+namespace PMS.Application.Services.User
+{
+    public interface IUserService
+    {
+        Task<Core.Domain.Identity.User?> GetUserById(string userId);
+        Task<IList<string>> GetUserRoles(Core.Domain.Identity.User user);
+        Task<ServiceResult<bool>> RegisterUserAsync(RegisterUser customer);
+        Task SendEmailConfirmAsync(Core.Domain.Identity.User user);
+        Task<ServiceResult<bool>> ReSendEmailConfirmAsync(ResendConfirmEmailRequest request);
+        Task<ServiceResult<bool>> ConfirmEmailAsync(string userId, string token);
+        Task<ServiceResult<bool>> SendEmailResetPasswordAsync(string email);
+        Task<ServiceResult<bool>> ResetPasswordAsync(ResetPasswordRequest request);
+    }
+}
