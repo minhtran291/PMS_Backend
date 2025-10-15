96fbd9abbd52bd96aa9289517faf2d56a360c250
minhtran291 <minhtran2912003@gmail.com>
2025-10-15 16:14:00 +0700

    update data seeder

---
+++ PMS.Application/DTOs/RequestSalesQuotation/CreateRsqDTO.cs
@@ -0,0 +1,15 @@
+﻿using PMS.Application.DTOs.RequestSalesQuotationDetails;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Application.DTOs.RequestSalesQuotation
+{
+    public class CreateRsqDTO
+    {
+        public int CustomerId { get; set; }
+        public List<CreateRsqDetailsDTO> RsqDetails { get; set; } = new List<CreateRsqDetailsDTO>();
+    }
+}
