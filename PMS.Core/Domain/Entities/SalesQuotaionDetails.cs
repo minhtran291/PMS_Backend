5da42fc66ef40b31776b0808b75b87c45e8c8850
minhtran291 <minhtran2912003@gmail.com>
2025-10-17 23:10:54 +0700

    Add entity sales quotation

---
+++ PMS.Core/Domain/Entities/SalesQuotaionDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Core.Domain.Entities
+{
+    public class SalesQuotaionDetails
+    {
+        public int Id { get; set; }
+        public int SqId { get; set; }
+        public int ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Tax { get; set; }
+    }
+}
---
+++ PMS.Core/Domain/Enums/SalesQuotationStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Core.Domain.Enums
+{
+    public enum SalesQuotationStatus
+    {
+        Draft = 0,
+        Sent = 1,
+        Expired = 2
+    }
+}
