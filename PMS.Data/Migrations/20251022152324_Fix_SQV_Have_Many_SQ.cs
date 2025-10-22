dfe2c424ae57a49be4d1e99e4382952aebc4e129
minhtran291 <minhtran2912003@gmail.com>
2025-10-22 22:50:40 +0700

    Fix context SQV, DTO sales quotation, add feature update sales quotation

---
+++ PMS.Data/Migrations/20251022152324_Fix_SQV_Have_Many_SQ.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_SQV_Have_Many_SQ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SalesQuotations_SqvId",
+                table: "SalesQuotations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesQuotations_SqvId",
+                table: "SalesQuotations",
+                column: "SqvId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SalesQuotations_SqvId",
+                table: "SalesQuotations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesQuotations_SqvId",
+                table: "SalesQuotations",
+                column: "SqvId",
+                unique: true);
+        }
+    }
+}
