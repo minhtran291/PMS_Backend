65774839b3888c4dc5d109ceee3707b584b7c58c
Anh Tran <161797047+hoanganh3321@users.noreply.github.com>
2025-10-22 23:23:54 +0700

    Feature/po (#54)
    * Purchasing Order Api endpoint developed
    
    * purchasing order update

---
+++ PMS.API/Controllers/POController.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PMS.Application.DTOs.PO;
+using PMS.Application.Services.PO;
+using PMS.Core.Domain.Constant;
+
+namespace PMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class POController : BaseController
+    {
+        private readonly IPOService _poService;
+        public POController(IPOService poService)
+        {
+            _poService = poService;
+        }
+
+        /// <summary>
+        /// https://localhost:7213/api/PO/getAllPo
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("getAllPo")]
+        [Authorize(Roles = $"{UserRoles.ACCOUNTANT},{UserRoles.PURCHASES_STAFF}")]
+        public async Task<IActionResult> GetAllPurchaseOrders()
+        {
+            var result = await _poService.GetAllPOAsync();
+            return HandleServiceResult(result);
+        }
+
+        /// <summary>
+        /// https://localhost:7213/api/PO/updatePo/{id}
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPut("updatePo/{poid}")]
+        [Authorize(Roles = UserRoles.ACCOUNTANT)]
+        public async Task<IActionResult> UpdatePurchaseOrder(int poid, [FromBody] POUpdateDTO dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new { Message = "Dữ liệu không hợp lệ.", Errors = ModelState });
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized(new { Message = "Không thể xác thực người dùng." });
+
+            var result = await _poService.UpdatePOAsync(userId, poid, dto);
+            return HandleServiceResult(result);
+        }
+
+        /// <summary>
+        /// Lấy chi tiết đơn mua hàng (PO) theo ID
+        /// https://localhost:7213/api/PO/GetPoDetailByPoId/poid
+        /// </summary>
+        [HttpGet("GetPoDetailByPoId/{poid}")]
+        [Authorize(Roles = $"{UserRoles.ACCOUNTANT},{UserRoles.PURCHASES_STAFF}")]
+        public async Task<IActionResult> GetPurchaseOrderDetail(int poid)
+        {
+            var result = await _poService.ViewDetailPObyID(poid);
+            return HandleServiceResult(result);
+        }
+    }
+}
---
+++ PMS.Application/DTOs/PO/POUpdateDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Application.DTOs.PO
+{
+    public class POUpdateDTO
+    {
+
+        public bool Status { get; set; }
+        [Required(ErrorMessage = "Deposit không được bỏ trống")]
+        [Range(0, double.MaxValue, ErrorMessage = "Số nhập phải lớn hơn 0")]
+        [Column(TypeName = "decimal(18, 2)")]
+        public required decimal Deposit { get; set; }
+        public decimal Debt { get; set; }
+        [Required(ErrorMessage = "Ngày trả không được bỏ trống")]
+        public required DateTime PaymentDate { get; set; }
+
+    }
+}
