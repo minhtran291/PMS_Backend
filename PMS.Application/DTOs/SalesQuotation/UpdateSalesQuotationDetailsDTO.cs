e838138083b9f81cc68bb22d00786ecdfa7c5da3
minhtran291 <minhtran2912003@gmail.com>
2025-10-30 17:36:46 +0700

    Fix update sales quotation

---
+++ PMS.Application/DTOs/SalesQuotation/UpdateSalesQuotationDetailsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Application.DTOs.SalesQuotation
+{
+    public class UpdateSalesQuotationDetailsDTO
+    {
+        public int sqdId {  get; set; }
+        public int? TaxId { get; set; }
+        public string? Note { get; set; }
+    }
+}
