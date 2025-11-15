60b59b6bc73bcedaf23333ceef54fc70dd061e09
Anh Tran <161797047+hoanganh3321@users.noreply.github.com>
2025-11-15 22:41:19 +0700

    Hoanganhdev (#173)

--- PMS.Application/DTOs/GRN/PODetailViewDTO.cs
+++ PMS.Application/DTOs/GRN/PODetailViewDTO.cs
@@ -1,27 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PMS.Application.DTOs.PO
+namespace PMS.Application.DTOs.GRN
 {
     public class PODetailViewDTO
-    {  
+    {
         public int PODID { get; set; }
-
         public int ProductID { get; set; }
         public string ProductName { get; set; }
         public string DVT { get; set; }
-        public int Quantity { get; set; }
+        public decimal Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal UnitPriceTotal { get; set; }
         public string Description { get; set; }
-        public int POID { get; set; }
-        public int RemainingQty { get; set; }
-        public DateTime ExpiredDate { get; set; }
-
+        public DateTime? ExpiredDate { get; set; }
+        public decimal RemainingQty { get; set; } 
     }
 }
