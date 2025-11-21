203ee9c440da28b6fdfb3c9ebbb87be66e257993
Anh Tran <161797047+hoanganh3321@users.noreply.github.com>
2025-11-21 22:47:27 +0700

    DebtwithPo

---
+++ PMS.Application/DTOs/PO/ViewDebtPODTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Application.DTOs.PO
+{
+    public class ViewDebtPODTO
+    {
+        public int poid { get; set; }
+        public decimal toatlPo { get; set; }
+    }
+}
