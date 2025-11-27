df57fd49e310b3cd14841e928c25fa4ba411d52d
Anh Tran <161797047+hoanganh3321@users.noreply.github.com>
2025-11-27 10:55:46 +0700

    Update Admin autho

---
+++ PMS.API/Helpers/AttributeRoles/PermissionAuthorizeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace PMS.API.Helpers.AttributeRoles
+{
+    public class PermissionAuthorizeAttribute : AuthorizeAttribute
+    {
+        public PermissionAuthorizeAttribute(string permission)
+        {
+            Policy = permission;
+        }
+    }
+}
