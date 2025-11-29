29a8d5463d39c1482fa8a0e919fd8950edc16213
hoanganh3321 <hoanganhfallinginlove@gmail.com>
2025-11-30 00:16:57 +0700

    Debt sup update

---
+++ PMS.Data/Migrations/20251129170545_qidupdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class qidupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentDueDate",
+                table: "Quotations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentDueDate",
+                table: "Quotations");
+        }
+    }
+}
