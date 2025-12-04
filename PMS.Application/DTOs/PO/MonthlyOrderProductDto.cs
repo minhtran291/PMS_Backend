f293751f37a3b277d0bc5498c0303599cbeddb7b
hoanganh3321 <hoanganhfallinginlove@gmail.com>
2025-12-04 23:21:40 +0700

    local

---
+++ PMS.Application/DTOs/PO/MonthlyOrderProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Application.DTOs.PO
+{
+    public class MonthlyOrderProductDto
+    {
+        public int Month { get; set; }
+        public List<OrderProductDetailDto> Products { get; set; } = new();
+    }
+}
---
+++ PMS.Application/DTOs/PO/PurchasingOrderStatisticDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Application.DTOs.PO
+{
+    public class PurchasingOrderStatisticDto
+    {
+        public int Year { get; set; }
+        public List<MonthlyOrderProductDto> MonthlyData { get; set; } = new();
+    }
+}
