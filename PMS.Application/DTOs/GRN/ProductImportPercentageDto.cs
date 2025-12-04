5b3ca967be2645e16c9fe8d286df8a9b131d0927
hoanganh3321 <hoanganhfallinginlove@gmail.com>
2025-12-04 20:14:28 +0700

    local

---
+++ PMS.Application/DTOs/GRN/ProductImportPercentageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PMS.Application.DTOs.GRN
+{
+    public class ProductImportPercentageDto
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Percentage { get; set; }
+    }
+}
